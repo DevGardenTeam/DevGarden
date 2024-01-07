a6f14e9863c42091a685b93a086b438adb148b8c
tilevadoux <tim.levadoux@etu.uca.fr>
2024-01-07 21:04:21 +0100

    debut page de projet

--- Sources/App.js
+++ Sources/App.js
@@ -1,40 +1,90 @@
-import { SafeAreaView, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, Switch, Dimensions, TouchableOpacity } from 'react-native';
 import { I18nextProvider, useTranslation } from "react-i18next"; // A ajouter pour le multi langue
+import i18n from './service/i18n';
+import React, { useState, useEffect } from 'react';
 
 export default function App() {
+  const [isDaytime, setIsDaytime] = useState(true);
+
+  useEffect(() => {
+    const currentHour = new Date().getHours();
+    const isDay = currentHour >= 6 && currentHour < 18;
+    setIsDaytime(isDay);
+  }, []); 
+
   const {t} = useTranslation();     // A ajouter pour le multi langue
+
+  var type = t('garden')
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.text}>{t('welcome')}</Text>  
-      <TouchableOpacity style={styles.button} onPress={() => setVisible(true)}>
-        <Text style={styles.buttonText}>{t('supportedLanguages.en')}</Text>
-      </TouchableOpacity>
+      <View style={isDaytime ? styles.day : styles.night}>
+        <View style={styles.top}>
+          <TouchableOpacity style={isDaytime ? styles.sun : styles.moon}/>
+          <View style={styles.switch}>
+            <Switch/>
+            <Text>{type}</Text>
+          </View>
+        </View>
+      </View>
+      <View style={styles.ground}> 
+      </View>
     </SafeAreaView>
   );
 }
 
-// t('[key]') => valeur directe dans le json
-// t('[key].[2nd_key]') => valeur indirecte dans le json
+const WIDTH = Dimensions.get('window').width ;
+const HEIGHT = Dimensions.get('window').height ;
+
+const ISLANDSCAPE = WIDTH > HEIGHT;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#191266',
+    backgroundColor: '#F1F0F0',
   },
-  button: {
-    backgroundColor: '#6258e8',
-    padding: 10,
-    borderRadius: 3,
+  ground:{
+    width: '100%',
+    height:'10%',
+    backgroundColor: "#7E6200"
   },
-  buttonText: {
-    color: 'white',
-    fontSize: 16,
+  day:{
+    backgroundColor: "#B1E3FF",
+    width: '100%',
+    height:'80%',
+    display: 'flex',
+    flexDirection:'column'
   },
-  text: {
-    marginBottom: 100,
-    fontSize: 18,
-    color: 'white',
+  night:{
+    backgroundColor: "#191970",
+    width: '100%',
+    height:'80%',
+    display: 'flex',
+    flexDirection:'column'
   },
+  top:{
+    width: '100%',
+    height:'10%',
+    justifyContent: "space-between",
+    flexDirection:'row'
+  },
+  switch: {
+    margin: ISLANDSCAPE ? "2%" : "5%",
+  },
+  sun:{
+    borderRadius: WIDTH,
+    height: ISLANDSCAPE ? HEIGHT*0.20 : WIDTH*0.25,
+    width: ISLANDSCAPE ? HEIGHT*0.20 : WIDTH*0.25,
+    backgroundColor: 'orange',
+    boxShadow: '0 0 5px rgba(255,127,0,1), 0 0 30px rgba(255,127,0,1), 0 0 100px rgba(255,255,0,1), inset 0 0 40px rgba(255,255,0,1)',
+    margin: ISLANDSCAPE ? "2%" : "5%",
+  },
+  moon:{
+    borderRadius: WIDTH,
+    margin: ISLANDSCAPE ? "2%" : "5%",
+    height: ISLANDSCAPE ? HEIGHT*0.20 : WIDTH*0.25,
+    width: ISLANDSCAPE ? HEIGHT*0.20 : WIDTH*0.25,
+    backgroundColor: '#fae4a8',
+    backgroundImage: 'radial-gradient(circle at 40% 80%, #ddc997 10%, transparent 0), radial-gradient(circle at 55% 70%, #ddc997 5%, transparent 0), radial-gradient(circle at 58% 85%, #ddc997 6%, transparent 0), radial-gradient(circle at 80% 40%, #ddc997 12%, transparent 0), radial-gradient(circle at 87% 58%, #ddc997 4%, transparent 0)',
+  }
 });
