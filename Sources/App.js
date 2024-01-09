d445bbc3bf25df04285f666ef2ed61f8643c7c3d
tilevadoux <tim.levadoux@etu.uca.fr>
2024-01-09 09:26:13 -0500

    debut ajout panneau et factorisation

--- Sources/App.js
+++ Sources/App.js
@@ -1,4 +1,4 @@
-import { SafeAreaView, StyleSheet, Text, View, Switch, Dimensions, TouchableOpacity } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, Switch, Dimensions, TouchableOpacity, Image } from 'react-native';
 import { I18nextProvider, useTranslation } from "react-i18next"; // A ajouter pour le multi langue
 import i18n from './service/i18n';
 import React, { useState, useEffect } from 'react';
@@ -18,17 +18,23 @@ export default function App() {
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={isDaytime ? styles.day : styles.night}>
+      <View style={[styles.days,isDaytime ? styles.day : styles.night]}>
         <View style={styles.top}>
-          <TouchableOpacity style={isDaytime ? styles.sun : styles.moon}/>
+          <TouchableOpacity style={[styles.luminary,isDaytime ? styles.sun : styles.moon]}/>
           <View style={styles.switch}>
             <Switch/>
             <Text>{type}</Text>
           </View>
         </View>
+        <View style={styles.bottom}>
+          <View style={styles.signview}>
+            <TouchableOpacity>
+              <Image source={require('./assets/panneau.png')}></Image>
+            </TouchableOpacity>
+          </View>
+        </View>
       </View>
       <View style={styles.ground}> 
-        <View style={styles.groundGradient}/>
       </View>
     </SafeAreaView>
   );
@@ -47,27 +53,20 @@ const styles = StyleSheet.create({
   ground:{
     width: '100%',
     height:'10%',
-    backgroundColor: 'linear-gradient(to bottom, #7E6200 75%, tra,sparent )'
+    backgroundImage: 'linear-gradient(to bottom, #7E6200 50%, transparent )'
     // 'linear-gradient(to bottom, #7E6200 75%, tra,sparent )'
   },
-  groundGradient:{
+  days: {
     width: '100%',
-    height:'100%',
-    backgroundColor: '#7E6200',
+    height:'80%',
+    display: 'flex',
+    flexDirection:'column'
   },
   day:{
     backgroundColor: "#B1E3FF",
-    width: '100%',
-    height:'80%',
-    display: 'flex',
-    flexDirection:'column'
   },
   night:{
     backgroundColor: "#191970",
-    width: '100%',
-    height:'80%',
-    display: 'flex',
-    flexDirection:'column'
   },
   top:{
     width: '100%',
@@ -78,20 +77,25 @@ const styles = StyleSheet.create({
   switch: {
     margin: ISLANDSCAPE ? "2%" : "5%",
   },
-  sun:{
+  luminary: {
     borderRadius: WIDTH,
     height: ISLANDSCAPE ? HEIGHT*0.20 : WIDTH*0.25,
     width: ISLANDSCAPE ? HEIGHT*0.20 : WIDTH*0.25,
-    backgroundColor: 'orange',
-    boxShadow: '0 0 5px rgba(255,127,0,1), 0 0 30px rgba(255,127,0,1), 0 0 100px rgba(255,255,0,1), inset 0 0 40px rgba(255,255,0,1)',
     margin: ISLANDSCAPE ? "2%" : "5%",
   },
+  sun:{
+    backgroundColor: 'orange',
+    boxShadow: '0 0 5px rgba(255,127,0,1), 0 0 30px rgba(255,127,0,1), 0 0 100px rgba(255,255,0,1), inset 0 0 40px rgba(255,255,0,1)',
+  },
   moon:{
-    borderRadius: WIDTH,
-    margin: ISLANDSCAPE ? "2%" : "5%",
-    height: ISLANDSCAPE ? HEIGHT*0.20 : WIDTH*0.25,
-    width: ISLANDSCAPE ? HEIGHT*0.20 : WIDTH*0.25,
     backgroundColor: '#fae4a8',
     backgroundImage: 'radial-gradient(circle at 40% 80%, #ddc997 10%, transparent 0), radial-gradient(circle at 55% 70%, #ddc997 5%, transparent 0), radial-gradient(circle at 58% 85%, #ddc997 6%, transparent 0), radial-gradient(circle at 80% 40%, #ddc997 12%, transparent 0), radial-gradient(circle at 87% 58%, #ddc997 4%, transparent 0)',
+  },
+  bottom:{
+    width: '100%',
+    flexDirection: 'row'
+  },
+  sign: {
+    resizeMode: 'contain'
   }
 });
