c95cab18acfbff2d21bd7eda602b1965a6bb475a
lou.broda <lou.broda@etu.uca.fr>
2024-01-15 11:18:13 +0100

    ADD : Début View all Repositories (not finished)

--- Sources/App.js
+++ Sources/App.js
@@ -1,6 +1,5 @@
-// App.js
-import React, { useEffect } from 'react';
-import { SafeAreaView, StyleSheet, View, Text, TouchableOpacity, FlatList, ActivityIndicator } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, StyleSheet, View, Text, TouchableOpacity, FlatList, ActivityIndicator, Dimensions, Button } from 'react-native';
 import { RepositoryController } from './view-controllers/RepositoryViewController';
 
 export default function App() {
@@ -9,6 +8,15 @@ export default function App() {
   useEffect(() => {
     fetchRepositories();
   }, []);
+
+  const [isDaytime, setIsDaytime] = useState(true);
+
+  useEffect(() => {
+    const currentHour = new Date().getHours();
+    const isDay = currentHour >= 5 && currentHour < 18;
+    setIsDaytime(isDay);
+  }, []); 
+
 
   if (loading) {
     return <ActivityIndicator size="large" />;
@@ -19,29 +27,71 @@ export default function App() {
   }
 
   return (
-    <View>
-      <Text>Repository List</Text>
+    <SafeAreaView style={styles.container2}>
+    <View style={styles.listTop}>
+      <View style={styles.switch}>
+        <Text style={[styles.text,isDaytime ? styles.textDay : styles.textNight]}>Choisissez un projet</Text>
+      </View>
+    </View>
+    <View style={styles.mainContent}>
       <FlatList
         data={repositories}
         keyExtractor={(item) => item.id.toString()}
         renderItem={({ item }) => (
           <TouchableOpacity onPress={() => handleRepositoryPress(item.id)}>
             <View>
-              <Text>{item.name}</Text>
-              <Text>{item.description}</Text>
+              <Button title={item.name}/>
             </View>
           </TouchableOpacity>
         )}
       />
     </View>
+  </SafeAreaView>
+
   );
 }
+
+const WIDTH = Dimensions.get('window').width ;
+const HEIGHT = Dimensions.get('window').height ;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F1F0F0',
+    flexDirection: 'column-reverse'
+  },
+  top:{
+    width: '100%',
+    justifyContent: "space-between",
+    flexDirection:'row',
+  },
+  textDay: {
+    color:'black'
+  },
+  textNight: {
+    color:'white'
+  },
+
+  // LIST VIEW
+  container2: {
+    flex: 1,
+    backgroundColor: '#F1F0F0',
+  },
+  listTop:{
+    flexDirection:'row',
+    justifyContent:'space-between',
+  },
+  mainContent:{
+    display: 'flex',
+    marginLeft: WIDTH/4,
+    marginRight: WIDTH/4,
+    height: HEIGHT,
     alignItems: 'center',
     justifyContent: 'center',
-  },
-});
+    backgroundColor: 'white',
+    borderColor: 'black',
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 20,
+  }
+})
