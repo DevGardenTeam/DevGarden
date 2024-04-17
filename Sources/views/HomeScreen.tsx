f577ac0472c32e3feca3c9b50af6dc6fb1c99580
brdacostac <Bruno.DA_COSTA_CUNHA@etu.uca.fr>
2024-04-17 12:37:32 +0200

    Model marche, utilisé gltf to react fiber pour que ça marche

--- Sources/views/HomeScreen.tsx
+++ Sources/views/HomeScreen.tsx
@@ -3,12 +3,16 @@ import { Canvas } from '@react-three/fiber/native'
 import { TreeModel } from '../components/3d_components/principal_tree_component'
 
 
+
 export default function App() {
   return (
-    <Canvas camera={{position : [-2, 3, 5], fov : 10}}>
+    <Canvas camera={ {position: [0, 3, 5]}}>
+      <directionalLight position={[10, 10, 5]} intensity={2} />
+      <directionalLight position={[-10, -10, -5]} intensity={2} />
       <Suspense>
         <TreeModel/>
       </Suspense>
+
     </Canvas>
   )
 }
