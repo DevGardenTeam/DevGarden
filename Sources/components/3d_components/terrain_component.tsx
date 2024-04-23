c514b3f51114101e6179a939c49bd55258587726
brdacostac <Bruno.DA_COSTA_CUNHA@etu.uca.fr>
2024-04-23 17:20:58 +0200

    Terrain marche, mais OrbitControl (pour la touche tactile) fait plainter le 3D sur mobile

--- Sources/components/3d_components/terrain_component.tsx
+++ Sources/components/3d_components/terrain_component.tsx
@@ -9,10 +9,10 @@ type GLTFResult = GLTF & {
       Plane: THREE.Mesh
     }
     materials: {
-      ['Dirt 02']: THREE.MeshStandardMaterial
+      Material: THREE.MeshStandardMaterial
       ['Material.003']: THREE.MeshStandardMaterial
     }
-}
+  }
 
 type Props = {}
 
@@ -20,23 +20,19 @@ export const TerrainModel = (props: Props) => {
 
     const { nodes, materials } = useGLTF(require('../../assets/3d/terrain.glb')) as GLTFResult
     return (
-        <group {...props} dispose={null} scale={0.}>
+        <group {...props} dispose={null} scale={0.3}>
         <mesh
-            castShadow
-            receiveShadow
-            geometry={nodes.Cube.geometry}
-            material={materials['Dirt 02']}
-            position={[0.529, 1.089, 0.546]}
-            scale={[6, 1, 6]}
+          geometry={nodes.Cube.geometry}
+          material={materials.Material}
+          position={[0.529, 1.089, 0.546]}
+          scale={[6, 1, 6]}
         />
         <mesh
-            castShadow
-            receiveShadow
-            geometry={nodes.Plane.geometry}
-            material={materials['Material.003']}
-            position={[0.529, 2.097, 0.546]}
-            scale={[6, 1, 6]}
+          geometry={nodes.Plane.geometry}
+          material={materials['Material.003']}
+          position={[0.529, 2.097, 0.546]}
+          scale={[6, 1, 6]}
         />
-        </group>
+      </group>
     )
 }
--- Sources/views/HomeScreen.tsx
+++ Sources/views/HomeScreen.tsx
@@ -1,23 +1,29 @@
 import { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber/native'
-import { TreeModel } from '../components/3d_components/principal_tree_component'
 import { TerrainModel } from '../components/3d_components/terrain_component'
+import useControls from 'r3f-native-orbitcontrols'
+import { View } from "react-native"
 
 
 
 export default function App() {
+  const [OrbitControls, events] = useControls()
   return (
-    <Canvas camera={ {position: [5, 3, 5]}}>
-      <directionalLight position={[1, 0, 0]} args={['white', 2]} />
-      <directionalLight position={[-1, 0, 0]} args={['white', 2]}  />
-      <directionalLight position={[0, 0, 1]} args={['white', 2]}  />
-      <directionalLight position={[0, -1, 0]} args={['white', 2]}  />
+
+    <View style={{ flex: 1 }} {...events}>
+      <Canvas camera={ {position: [5, 3, 5]}}>
+        {/* <OrbitControls enablePan={false}/> Cela fait plainter le 3D à voir */}
+        <directionalLight position={[1, 0, 0]} args={['white', 2]} />
+        <directionalLight position={[-1, 0, 0]} args={['white', 2]}  />
+        <directionalLight position={[0, 0, 1]} args={['white', 2]}  />
+        <directionalLight position={[0, -1, 0]} args={['white', 2]}  />
 
 
-      <ambientLight intensity={5} />
-      <Suspense>
-        <TerrainModel/>
-      </Suspense>
-    </Canvas>
+        <Suspense fallback={null}>
+          <TerrainModel/>
+        </Suspense>
+      </Canvas>
+    </View>
   )
+
 }
