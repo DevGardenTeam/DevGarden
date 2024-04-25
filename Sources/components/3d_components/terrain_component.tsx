0d695bf2bbfa770f824b3efb5ca12bef1ff41612
Bruno Da Costa Cunha <Bruno.DA_COSTA_CUNHA@etu.uca.fr>
2024-04-25 16:08:32 +0200

    OrbitControls marche

--- Sources/components/3d_components/terrain_component.tsx
+++ Sources/components/3d_components/terrain_component.tsx
@@ -1,18 +1,22 @@
 import * as THREE from 'three'
 import React, { useRef } from 'react'
-import { useGLTF } from '@react-three/drei/native'
+import { Float, useGLTF } from '@react-three/drei/native'
 import { GLTF } from 'three-stdlib'
 
+
 type GLTFResult = GLTF & {
-    nodes: {
-      Cube: THREE.Mesh
-      Plane: THREE.Mesh
-    }
-    materials: {
-      Material: THREE.MeshStandardMaterial
-      ['Material.003']: THREE.MeshStandardMaterial
-    }
+  nodes: {
+    Cube_1: THREE.Mesh
+    Cube_2: THREE.Mesh
+    Cube001_1: THREE.Mesh
+    Cube001_2: THREE.Mesh
   }
+  materials: {
+    Dirt: THREE.MeshStandardMaterial
+    Water: THREE.MeshStandardMaterial
+    ['Grass.001']: THREE.MeshStandardMaterial
+  }
+}
 
 type Props = {}
 
@@ -21,18 +25,29 @@ export const TerrainModel = (props: Props) => {
     const { nodes, materials } = useGLTF(require('../../assets/3d/terrain.glb')) as GLTFResult
     return (
         <group {...props} dispose={null} scale={0.3}>
-        <mesh
-          geometry={nodes.Cube.geometry}
-          material={materials.Material}
-          position={[0.529, 1.089, 0.546]}
-          scale={[6, 1, 6]}
-        />
-        <mesh
-          geometry={nodes.Plane.geometry}
-          material={materials['Material.003']}
-          position={[0.529, 2.097, 0.546]}
-          scale={[6, 1, 6]}
-        />
+          <group position={[0, -0.16, 0]} scale={[6, 1, 6]}>
+            <mesh castShadow receiveShadow geometry={nodes.Cube_1.geometry} material={materials.Dirt} />
+            <mesh
+              castShadow
+              receiveShadow
+              geometry={nodes.Cube_2.geometry}
+              material={materials.Water}
+            />
+          </group>
+          <group position={[0, 0.837, 0]} scale={[6.648, 0.243, 6.648]}>
+            <mesh
+              castShadow
+              receiveShadow
+              geometry={nodes.Cube001_1.geometry}
+              material={materials.Dirt}
+            />
+            <mesh
+              castShadow
+              receiveShadow
+              geometry={nodes.Cube001_2.geometry}
+              material={materials['Grass.001']}
+            />
+          </group>
       </group>
     )
 }
--- Sources/views/HomeScreen.tsx
+++ Sources/views/HomeScreen.tsx
@@ -1,28 +1,38 @@
-import { Suspense } from 'react'
+import { Suspense, useState } from 'react'
 import { Canvas } from '@react-three/fiber/native'
 import { TerrainModel } from '../components/3d_components/terrain_component'
-import useControls from 'r3f-native-orbitcontrols'
 import { View } from "react-native"
+import Trigger from '../components/3d_components/trigger';
+import Loader from '../components/3d_components/loader';
+import useControls from "r3f-native-orbitcontrols"
 
 
 
-export default function App() {
-  const [OrbitControls, events] = useControls()
+export default function HomeScreen() {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [OrbitControls ,events] = useControls()
+
+
   return (
 
-    <View style={{ flex: 1 }} {...events}>
-      <Canvas camera={ {position: [5, 3, 5]}}>
-        {/* <OrbitControls enablePan={false}/> Cela fait plainter le 3D à voir */}
+    <View style={{ flex: 1 }} {...events} >
+                  {loading && <Loader />}
+      <Canvas frameloop="demand" camera={ {position: [5, 3, 5]}}>
+
+        <OrbitControls enablePan={false}/>
+
         <directionalLight position={[1, 0, 0]} args={['white', 2]} />
         <directionalLight position={[-1, 0, 0]} args={['white', 2]}  />
         <directionalLight position={[0, 0, 1]} args={['white', 2]}  />
+        <directionalLight position={[0, 1, 0]} args={['white', 2]}  />
         <directionalLight position={[0, -1, 0]} args={['white', 2]}  />
 
 
-        <Suspense fallback={null}>
+        <Suspense fallback={<Trigger setLoading={setLoading} />}>
           <TerrainModel/>
         </Suspense>
       </Canvas>
+      
     </View>
   )
 
