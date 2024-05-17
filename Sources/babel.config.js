96f1da139dcaf444b2117dd71f6fb7da0138b194
nico-dev <nico-dev@T480S>
2024-05-17 14:57:20 +0200

    :construction: expo version working (mobile)

--- Sources/babel.config.js
+++ Sources/babel.config.js
@@ -3,5 +3,8 @@ module.exports = {
     plugins: [
       // Your other plugins
       'react-native-reanimated/plugin',
+      ['@babel/plugin-transform-class-properties', { loose: true }],
+      ['@babel/plugin-transform-private-methods', { loose: true }],
+      ['@babel/plugin-transform-private-property-in-object', { loose: true }]
     ],
   };
