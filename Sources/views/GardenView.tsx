d090813146a1f34825de922b4e163acf427b63ed
nico-dev <nico-dev@T480S>
2024-05-19 16:37:13 +0200

    :art: custom bg

--- Sources/views/GardenView.tsx
+++ Sources/views/GardenView.tsx
@@ -1,11 +1,11 @@
 import React, { useEffect } from 'react';
-import Svg, { Circle, Rect } from 'react-native-svg';
+import Svg, { Circle, Rect, Image } from 'react-native-svg';
 import { Gesture, GestureDetector, GestureHandlerRootView, } from 'react-native-gesture-handler';
 import Animated, { useSharedValue, useAnimatedStyle } from 'react-native-reanimated';
-
-import { Dimensions } from 'react-native';
+import { Dimensions, View, StyleSheet } from 'react-native';
 
 import { Tree, generateTrees } from '../components/garden_view/tree';
+import CustomSvg from '../assets/garden_themes/gardenbg.svg'
 
 const MAX_SCALE = 2.5; // Maximum zoom level
 const NUMBER_OF_GARDENS = 3; // Number of gardens
@@ -16,6 +16,8 @@ const GardenView: React.FC = () => {
     const windowDimensions = Dimensions.get('window');
     const containerWidth = windowDimensions.width;
     const containerHeight = windowDimensions.height - 50;
+
+    console.log("width: ", containerWidth, "height: ", containerHeight)
 
     const svgWidth = containerWidth * NUMBER_OF_GARDENS; // Width of the SVG content
     const svgHeight = containerHeight; // Height of the SVG content
@@ -60,7 +62,7 @@ const GardenView: React.FC = () => {
             // Calculate bounds for translation
             const maxTranslateX = ((scaledSvgWidth - containerWidth) / 2 / scale.value) - containerWidth;
             const minTranslateX = -(maxTranslateX + containerWidth * (NUMBER_OF_GARDENS - 1));
-            //console.log("min: ", minTranslateX, "max: ", maxTranslateX)
+            console.log("min: ", minTranslateX, "max: ", maxTranslateX)
 
             const maxTranslateY = (scaledSvgHeight - containerHeight) / 2 / scale.value;
             const minTranslateY = -maxTranslateY;
@@ -91,21 +93,44 @@ const GardenView: React.FC = () => {
         <GestureHandlerRootView  style={{ flex: 1, backgroundColor: '#40A578' }}>
             <GestureDetector gesture={combinedGesture}>
             <Animated.View style={[{ flex: 1 }, animatedStyle]}>
+                
                 <Svg height={svgHeight} width={svgWidth}>
 
                     {/* garden 1 */}
-                    <Rect x="0" width={containerWidth} height={svgHeight} fill="blue" stroke="red" strokeWidth="2"/>
-                    
+                    <Image
+                    x="0"
+                    y="0"
+                    width={containerWidth}
+                    height={svgHeight}
+                    href={require('../assets/garden_themes/garden1bg.png')}
+                    preserveAspectRatio="xMidYMid slice"
+                    />
+
                     {/* garden 2 */}
-                    <Rect x={containerWidth} width={containerWidth} height={svgHeight} fill="green" stroke="red" strokeWidth="2"/>
+                    <Image
+                    x={containerWidth}
+                    y="0"
+                    width={containerWidth}
+                    height={svgHeight}
+                    href={require('../assets/garden_themes/garden2bg.png')}
+                    preserveAspectRatio="xMidYMid slice"
+                    />
 
                     {/* garden 3 */}
-                    <Rect x={containerWidth*2} width={containerWidth} height={svgHeight} fill="orange" stroke="red" strokeWidth="2"/>
+                    <Image
+                    x={containerWidth*2}
+                    y="0"
+                    width={containerWidth}
+                    height={svgHeight}
+                    href={require('../assets/garden_themes/garden1bg.png')}
+                    preserveAspectRatio="xMidYMid slice"
+                    />
 
                     {trees.map(tree => tree.visible && (
                             <Circle key={tree.label} cx={tree.x.toString()} cy={tree.y.toString()} r={tree.radius.toString()} fill="#40A578" />
                         ))}
                 </Svg>
+              
             </Animated.View>
             </GestureDetector>
       </GestureHandlerRootView >
@@ -113,3 +138,12 @@ const GardenView: React.FC = () => {
   };
 
 export default GardenView;
+
+{/* <Svg height={svgHeight} width={svgWidth}>
+
+<Rect x="0" width={containerWidth} height={svgHeight} fill="blue" stroke="red" strokeWidth="2"/>
+
+<Rect x={containerWidth} width={containerWidth} height={svgHeight} fill="green" stroke="red" strokeWidth="2"/>
+
+<Rect x={containerWidth*2} width={containerWidth} height={svgHeight} fill="orange" stroke="red" strokeWidth="2"/>
+</Svg> */}
