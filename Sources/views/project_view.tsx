c7037b803df0e6b39a0a07e87d5057162f116c8b
brdacostac <Bruno.DA_COSTA_CUNHA@etu.uca.fr>
2024-06-04 16:41:21 +0200

    feat : new 3D burned model component Add new 3D model component for bad(burned) terrain

--- Sources/views/project_view.tsx
+++ Sources/views/project_view.tsx
@@ -1,7 +1,7 @@
 import { SafeAreaView, StyleSheet, Text, View, Switch, Dimensions, ImageStyle, ScrollView, StatusBar } from 'react-native';
 import { useTranslation } from "react-i18next"; // A ajouter pour le multi langue
 import '../service/i18n';
-import React, { useState, Suspense, useEffect } from 'react';
+import React, { useState, Suspense } from 'react';
 import NavigationButton from '../components/button_component'
 import BackNavigationButton from '../components/button_back_navigation_component';
 import { StackNavigationProp } from '@react-navigation/stack';
@@ -15,11 +15,10 @@ import useControls from "r3f-native-orbitcontrols"
 import { Canvas } from '@react-three/fiber/native'
 import { TerrainModel } from '../components/3d_components/terrain_component'
 import { TerrainModel2 } from '../components/3d_components/terrain2_component'
+import { TerrainModel3 } from '../components/3d_components/terrain3_component'
 
 
-interface CustomStyle extends ImageStyle {
-  backgroundImage?: string;
-}
+
 
 
 interface ProjectScreenProps {
@@ -36,19 +35,8 @@ const ProjectScreen: React.FC<ProjectScreenProps> = ({ navigation }) => {
   const route = useRoute();
   const { platform, owner, repository } = route.params as RouteParams;
   const { colors } = useTheme();
-  const [selectedPlatform] = useState<string | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
   const [OrbitControls ,events] = useControls()
-
-
-  // Changement de fond selon l'horaire
-  const [isDaytime, setIsDaytime] = useState(true);
-
-  useEffect(() => {
-    const currentHour = new Date().getHours();
-    const isDay = currentHour >= 8 && currentHour < 18;
-    setIsDaytime(isDay);
-  }, []); 
 
   // Switch
 
@@ -59,7 +47,7 @@ const ProjectScreen: React.FC<ProjectScreenProps> = ({ navigation }) => {
     setView(!view); // Ajoutez cette ligne pour mettre à jour la variable view
   };
 
-  const {t} = useTranslation();     // A ajouter pour le multi langue
+  const {t} = useTranslation();  // A ajouter pour le multi langue
 
   if (!view) {
     const type = t('projectView.list');
@@ -99,9 +87,7 @@ const ProjectScreen: React.FC<ProjectScreenProps> = ({ navigation }) => {
 
   return (
     <SafeAreaView style={{ backgroundColor: colors.background,height: "100%" }}>
-    <LinearGradient colors={isDaytime ? [ '#2B75B4', '#5292C5','#93C3E1','#C4E5F4','#DFF6FC'] : [ '#2654AC', '#4674DC','#325EBF','#173B88','#091434']}
-        locations={isDaytime ? [0,0.2,0.5,0.8,1] :[0,0.2,0.4,0.6,1]}
-        style={[styles.days]}>
+    <LinearGradient colors={['#2B75B4', '#5292C5','#93C3E1','#C4E5F4','#DFF6FC']} style={[styles.days]}>
       <View style={styles.topList}>
         <View style={styles.navigationBack}>
           <BackNavigationButton onPress={() => navigation.goBack()}/> 
@@ -123,31 +109,24 @@ const ProjectScreen: React.FC<ProjectScreenProps> = ({ navigation }) => {
       <View style={{ flex: 1,  }} {...events} >
                     {loading && <Loader />}
                    
-
         <Canvas frameloop="demand" camera={{ position: [5, 3, 5] }}>
-          <OrbitControls enablePan={false} maxZoom={10} minZoom={3.5}
+          <OrbitControls enablePan={false} maxZoom={7} minZoom={3.5}
             maxPolarAngle={1.8} minPolarAngle={0.5} maxAzimuthAngle={3} minAzimuthAngle={-0.1} />
-            
+          <ambientLight intensity={1} />
           <directionalLight position={[1, 0, 0]} args={['white', 2]} />
           <directionalLight position={[0, 1, 0]} args={['white', 2]}  />
           <directionalLight position={[0, 0, 1]} args={['white', 2]}  />
-          <directionalLight position={[1, 1, 1]} args={['white', 1]}  />
-
-
           <Suspense fallback={<Trigger setLoading={setLoading} />}>
-            <TerrainModel2 
-                onClickTree={() => navigation.navigate("AllCommits", {platform: platform, owner: owner, repository: repository})}
-                onClickChest={() => navigation.navigate("AllFiles", {platform: platform, owner: owner, repository: repository})}
-                onClickSign={() => navigation.navigate("Dashboard", {platform: platform, owner: owner, repository: repository})}
-                onClickBush={() => navigation.navigate("AllIssues", {platform: platform, owner: owner, repository: repository})}
-                onClickRock={() => navigation.navigate("ProjectManagement", {platform: platform, owner: owner, repository: repository})}
+            <TerrainModel3 
+              onClickTree={() => navigation.navigate("AllCommits", {platform: platform, owner: owner, repository: repository})}
+              onClickChest={() => navigation.navigate("AllFiles", {platform: platform, owner: owner, repository: repository})}
+              onClickSign={() => navigation.navigate("Dashboard", {platform: platform, owner: owner, repository: repository})}
+              onClickBush={() => navigation.navigate("AllIssues", {platform: platform, owner: owner, repository: repository})}
+              onClickRock={() => navigation.navigate("ProjectManagement", {platform: platform, owner: owner, repository: repository})}
             />
           </Suspense>
         </Canvas>
-        
       </View>    
-      
-
       </LinearGradient>
     </SafeAreaView>
   );
