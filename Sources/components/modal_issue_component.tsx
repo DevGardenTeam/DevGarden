ba07b51352bc9a70a1775afdb7febe6d77e98176
lou.broda <lou.broda@etu.uca.fr>
2024-06-12 15:14:36 +0200

    ADD : ajout de filtres dans issues et commits

--- Sources/components/modal_issue_component.tsx
+++ Sources/components/modal_issue_component.tsx
@@ -1,118 +1,124 @@
 import React from 'react';
 import { View, Image, StyleSheet, Text, TouchableOpacity } from 'react-native';
 import { useTheme } from '@react-navigation/native';
+import fontSizes from '../constants/fontSize';
+
 
 type ModalIssueComponentProps = {
-    id: string;
-    title: string;
-    body: string;
-    state: string;
-    creationDate: string;
-    author: string;
-    onSelect: () => void;
+   id: string;
+   title: string;
+   body: string;
+   state: string;
+   creationDate: string;
+   author: string;
+   onSelect: () => void;
 };
+
 
 const ModalIssueComponent: React.FC<ModalIssueComponentProps> = ({ id, title, body, state, creationDate, author, onSelect }) => {
-    const { colors } = useTheme();
+   const { colors } = useTheme();
 
-    return (
-        <View style={styles.modalView}>
-            <TouchableOpacity onPress={onSelect}>
-                <Image source={require('../assets/icons/close.png')} style={[styles.closeImage, { tintColor: colors.text }]} />
-            </TouchableOpacity>
-            <View style={styles.topModalBar}>
-                <View style={styles.userInfo}>
-                    <View style={styles.squareContainer}>
-                        <View style={[styles.statusIndicator, state === 'open' ? styles.openStatus : styles.closedStatus]} />
-                    </View>
-                    <View style={styles.userNameDate}>
-                        <Text style={[styles.userName, { color: colors.text }]}>{title}</Text>
-                        <Text style={[styles.date, { color: colors.text }]}>{creationDate}</Text>
-                    </View>
-                </View>
-            </View>
-            <Text style={[styles.message, { color: colors.text }]}>{body}</Text>
-            <View style={styles.branchInfo}>
-                <Text style={[styles.branch, { color: colors.text }]}>Author: {author}</Text>
-                <Text style={[styles.commitId, { color: colors.text }]}>Issue ID: {id}</Text>
-            </View>
-        </View>
-    );
+
+   return (
+       <View style={styles.modalView}>
+           <TouchableOpacity onPress={onSelect}>
+               <Image source={require('../assets/icons/chevron_down.png')} style={[styles.closeImage, { tintColor: colors.text }]} />
+           </TouchableOpacity>
+           <View style={styles.topModalBar}>
+               <View style={styles.userInfo}>
+                   <View style={styles.squareContainer}>
+                       <View style={[styles.statusIndicator, state === 'open' ? styles.openStatus : styles.closedStatus]} />
+                   </View>
+                   <View style={styles.userNameDate}>
+                       <Text style={[styles.userName, { color: colors.text }]}>{title}</Text>
+                       <Text style={[styles.date, { color: colors.text }]}>{creationDate}</Text>
+                   </View>
+               </View>
+           </View>
+           <Text style={[styles.message, { color: colors.text }]}>{body}</Text>
+           <View style={styles.branchInfo}>
+               <Text style={[styles.branch, { color: colors.text }]}>Author: {author}</Text>
+               <Text style={[styles.commitId, { color: colors.text }]}>Issue ID: {id}</Text>
+           </View>
+       </View>
+   );
 };
 
+
 const styles = StyleSheet.create({
-    modalView: {
-        flex: 1,
-        padding: 20,
-        backgroundColor: 'white',
-        borderTopLeftRadius: 20,
-        borderTopRightRadius: 20,
-        alignItems: 'center',
-        justifyContent: 'space-between',
-    },
-    topModalBar: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        width: '100%',
-        marginBottom: 20,
-    },
-    userInfo: {
-        flexDirection: 'row',
-        alignItems: 'center',
-    },
-    squareContainer: {
-        width: 50,
-        height: 50,
-        borderRadius: 25,
-        overflow: 'hidden',
-        marginRight: 10,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    statusIndicator: {
-        width: 30,
-        height: 30,
-        borderRadius: 15,
-    },
-    openStatus: {
-        backgroundColor: 'green',
-    },
-    closedStatus: {
-        backgroundColor: 'red',
-    },
-    userNameDate: {
-        justifyContent: 'center',
-    },
-    userName: {
-        fontSize: 18,
-        fontWeight: 'bold',
-    },
-    date: {
-        fontSize: 14,
-        color: 'gray',
-    },
-    closeImage: {
-        width: 30,
-        height: 30,
-    },
-    message: {
-        flex: 1,
-        fontSize: 16,
-        marginBottom: 20,
-    },
-    branchInfo: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        width: '100%',
-    },
-    branch: {
-        fontSize: 16,
-    },
-    commitId: {
-        fontSize: 16,
-        color: 'gray',
-    },
+   modalView: {
+       flex: 1,
+       padding: 20,
+       backgroundColor: 'white',
+       borderTopLeftRadius: 20,
+       borderTopRightRadius: 20,
+       alignItems: 'center',
+       justifyContent: 'space-between',
+   },
+   topModalBar: {
+       flexDirection: 'row',
+       justifyContent: 'space-between',
+       alignItems: 'center',
+       width: '100%',
+       marginBottom: 20,
+   },
+   userInfo: {
+       flexDirection: 'row',
+       alignItems: 'center',
+   },
+   squareContainer: {
+       width: fontSizes.iconMedium,
+       height: fontSizes.iconMedium,
+       borderRadius: fontSizes.iconSmall,
+       overflow: 'hidden',
+       marginRight: 10,
+       justifyContent: 'center',
+       alignItems: 'center',
+   },
+   statusIndicator: {
+       width: fontSizes.iconMedium,
+       height: fontSizes.iconMedium,
+       borderRadius: fontSizes.iconSmall,
+   },
+   openStatus: {
+       backgroundColor: 'green',
+   },
+   closedStatus: {
+       backgroundColor: 'red',
+   },
+   userNameDate: {
+       justifyContent: 'center',
+   },
+   userName: {
+       fontSize: fontSizes.large,
+       fontWeight: 'bold',
+   },
+   date: {
+       fontSize: fontSizes.small,
+       color: 'gray',
+   },
+   closeImage: {
+       width: fontSizes.iconSmall,
+       height: fontSizes.iconSmall,
+   },
+   message: {
+       flex: 1,
+       fontSize: fontSizes.medium,
+       marginBottom: 20,
+   },
+   branchInfo: {
+       flexDirection: 'row',
+       justifyContent: 'space-between',
+       width: '100%',
+   },
+   branch: {
+       fontSize: fontSizes.medium,
+   },
+   commitId: {
+       fontSize: fontSizes.medium,
+       color: 'gray',
+   },
 });
+
 
 export default ModalIssueComponent;
--- Sources/views/all_commits_view.tsx
+++ Sources/views/all_commits_view.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { SafeAreaView, StyleSheet, View, Text, TouchableOpacity, Image, FlatList, ActivityIndicator, StatusBar } from 'react-native';
+import { SafeAreaView, StyleSheet, View, Text, TouchableOpacity, Image, FlatList, ActivityIndicator, StatusBar, TextInput } from 'react-native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { useRoute } from '@react-navigation/native';
 import BackNavigationButton from '../components/button_back_navigation_component';
@@ -13,191 +13,279 @@ import { Repository } from '../model/Repository';
 import Modal from 'react-native-modal';
 import fontSizes from '../constants/fontSize';
 
+
 interface AllCommitsViewProps {
-  navigation: StackNavigationProp<any>;
+ navigation: StackNavigationProp<any>;
 }
+
 
 interface RouteParams {
-  repository: Repository;
+ repository: Repository;
 }
 
+
 const AllCommitsView: React.FC<AllCommitsViewProps> = ({ navigation }) => {
-  const route = useRoute();
-  const { repository } = route.params as RouteParams;
-  const { colors } = useTheme();
+ const route = useRoute();
+ const { repository } = route.params as RouteParams;
+ const { colors } = useTheme();
 
-  const [isModalVisible, setModalVisible] = useState(false);
-  const [selectedItem, setSelectedItem] = useState<null | Commit>(null);
 
-  const { commits, loading, error, handleCommitPress, getAllCommits } = CommitViewController({ platform: repository.platform, owner: repository.owner.name, repository: repository.name });
+ const [isModalVisible, setModalVisible] = useState(false);
+ const [selectedItem, setSelectedItem] = useState<null | Commit>(null);
+ const [searchText, setSearchText] = useState('');
+ const [sortOrder, setSortOrder] = useState<'asc' | 'desc'>('desc');
 
-  useEffect(() => {
-    getAllCommits();
-  }, []);
 
-  if (loading) {
-    return <ActivityIndicator size="large" />;
-  }
+ const { commits, loading, error, handleCommitPress, getAllCommits } = CommitViewController({ platform: repository.platform, owner: repository.owner.name, repository: repository.name });
 
-  if (error) {
-    return <Text>Error: {error}</Text>;
-  }
 
-  return (
-    <SafeAreaView style={[styles.safeAreaView, { backgroundColor: colors.background }]}>
-      <View style={styles.top}>
-        <View style={styles.navigationBack}>
-          <BackNavigationButton />
-        </View>
-        <View style={styles.titleContainer}>
-          <Text style={[styles.titleText, { color: colors.text }]}>Commits</Text>
-          <Text style={[styles.titleTextBis, { color: colors.text }]}> (master)</Text>
-        </View>
-      </View>
+ useEffect(() => {
+   getAllCommits();
+ }, []);
 
-      <View style={styles.mainView}>
-        <View style={styles.contentView}>
-          <FlatList
-            style={styles.flatList}
-            data={commits}
-            keyExtractor={(item) => item.id.toString()}
-            renderItem={({ item }) => (
-              <View style={styles.masterLabel}>
-                <TouchableOpacity onPress={() => {
-                  setModalVisible(true);
-                  setSelectedItem(item);
-                }}>
-                  <View style={styles.itemContainer}>
-                    <View style={styles.textInfoContainer}>
-                      <Text style={[styles.commitMessage, { color: colors.text }]} numberOfLines={1} ellipsizeMode="tail">
-                        {item.message}
-                      </Text>
-                      <Text style={[styles.commitAuthor, { color: colors.text }]}>
-                        {item.author.name}
-                      </Text>
-                      <Text style={[styles.commitAuthor, { color: colors.text }]}>
-                        {item.date ? DateUtils.formatDate(item.date.toString()) : ''}
-                      </Text>
-                    </View>
-                    <Image
-                      source={require('../assets/icons/right_arrow.png')}
-                      style={[styles.icon, { tintColor: colors.text }]}
-                    />
-                  </View>
-                </TouchableOpacity>
-              </View>
-            )}
-            showsVerticalScrollIndicator={false}
-          />
-        </View>
-        <Modal
-          style={styles.modalContainer}
-          isVisible={isModalVisible}
-          onSwipeComplete={() => { setModalVisible(false); setSelectedItem(null); }}
-          swipeDirection="down"
-          onBackdropPress={() => { setModalVisible(false); setSelectedItem(null); }}
-          onBackButtonPress={() => { setModalVisible(false); setSelectedItem(null); }}
-          backdropOpacity={0.5}
-        >
-          <View style={styles.modalView}>
-            <ModalCommitComponent
-              image={selectedItem?.author.photoUrl ?? ''}
-              username={selectedItem?.author.name ?? ''}
-              date={selectedItem?.date ? DateUtils.formatDate(selectedItem.date.toString()) : ''}
-              message={selectedItem?.message ?? ''}
-              branch={selectedItem?.id ?? ''}
-              id={selectedItem?.id ?? ''}
-              onSelect={() => {
-                setModalVisible(false);
-                setSelectedItem(null);
-              }}
-            />
-          </View>
-        </Modal>
-      </View>
-    </SafeAreaView>
-  );
+
+ const filteredCommits = commits
+   .filter(commit =>
+     commit.message?.toLowerCase().includes(searchText.toLowerCase()) ||
+     commit.author.name?.toLowerCase().includes(searchText.toLowerCase())
+   )
+   .sort((a, b) => {
+     if (sortOrder === 'asc') {
+       return new Date(a.date).getTime() - new Date(b.date).getTime();
+     } else {
+       return new Date(b.date).getTime() - new Date(a.date).getTime();
+     }
+   });
+
+
+ if (loading) {
+   return <ActivityIndicator size="large" />;
+ }
+
+
+ if (error) {
+   return <Text>Error: {error}</Text>;
+ }
+
+
+ return (
+   <SafeAreaView style={[styles.safeAreaView, { backgroundColor: colors.background }]}>
+     <View style={styles.top}>
+       <View style={styles.navigationBack}>
+         <BackNavigationButton />
+       </View>
+       <View style={styles.titleContainer}>
+         <Text style={[styles.titleText, { color: colors.text }]}>Commits</Text>
+         <Text style={[styles.titleTextBis, { color: colors.text }]}> (master)</Text>
+       </View>
+       <View style={styles.navigationPlaceholder} />
+     </View>
+
+
+     <View style={styles.searchContainer}>
+       <TextInput
+         style={[styles.searchInput, { color: colors.text, borderColor: colors.border }]}
+         placeholder="Search commits..."
+         placeholderTextColor={colors.text}
+         value={searchText}
+         onChangeText={setSearchText}
+       />
+       <View style={styles.sortContainer}>
+         <TouchableOpacity
+           style={[styles.sortButton, sortOrder === 'asc' && styles.selectedSort]}
+           onPress={() => setSortOrder('asc')}
+         >
+           <Text style={styles.sortText}>Asc</Text>
+         </TouchableOpacity>
+         <TouchableOpacity
+           style={[styles.sortButton, sortOrder === 'desc' && styles.selectedSort]}
+           onPress={() => setSortOrder('desc')}
+         >
+           <Text style={styles.sortText}>Desc</Text>
+         </TouchableOpacity>
+       </View>
+     </View>
+
+
+     <View style={styles.mainView}>
+       <View style={styles.contentView}>
+         <FlatList
+           style={styles.flatList}
+           data={filteredCommits}
+           keyExtractor={(item) => item.id.toString()}
+           renderItem={({ item }) => (
+             <View style={styles.masterLabel}>
+               <TouchableOpacity onPress={() => {
+                 setModalVisible(true);
+                 setSelectedItem(item);
+               }}>
+                 <View style={styles.itemContainer}>
+                   <View style={styles.textInfoContainer}>
+                     <Text style={[styles.commitMessage, { color: colors.text }]} numberOfLines={1} ellipsizeMode="tail">
+                       {item.message ?? ''}
+                     </Text>
+                     <Text style={[styles.commitAuthor, { color: colors.text }]}>
+                       {item.author.name ?? ''}
+                     </Text>
+                     <Text style={[styles.commitDate, { color: colors.text }]}>
+                       {item.date ? DateUtils.formatDate(item.date.toString()) : ''}
+                     </Text>
+                   </View>
+                   <Image
+                     source={require('../assets/icons/right_arrow.png')}
+                     style={[styles.icon, { tintColor: colors.text }]}
+                   />
+                 </View>
+               </TouchableOpacity>
+             </View>
+           )}
+           showsVerticalScrollIndicator={false}
+         />
+       </View>
+       <Modal
+         style={styles.modalContainer}
+         isVisible={isModalVisible}
+         onSwipeComplete={() => { setModalVisible(false); setSelectedItem(null); }}
+         swipeDirection="down"
+         onBackdropPress={() => { setModalVisible(false); setSelectedItem(null); }}
+         onBackButtonPress={() => { setModalVisible(false); setSelectedItem(null); }}
+         backdropOpacity={0.5}
+       >
+         <View style={styles.modalView}>
+           <ModalCommitComponent
+             image={selectedItem?.author.photoUrl ?? ''}
+             username={selectedItem?.author.name ?? ''}
+             date={selectedItem?.date ? DateUtils.formatDate(selectedItem.date.toString()) : ''}
+             message={selectedItem?.message ?? ''}
+             branch={selectedItem?.id ?? ''}
+             id={selectedItem?.id ?? ''}
+             onSelect={() => {
+               setModalVisible(false);
+               setSelectedItem(null);
+             }}
+           />
+         </View>
+       </Modal>
+     </View>
+   </SafeAreaView>
+ );
 };
 
+
 const styles = StyleSheet.create({
-  safeAreaView: {
-    flex: 1,
-  },
-  top: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    marginTop: StatusBar.currentHeight || 0,
-    marginBottom: verticalScale(15)
-  },
-  navigationBack: {
-    position: "absolute",
-    top: verticalScale(15),
-    left: horizontalScale(15),
-    zIndex: 1,
-  },
-  titleContainer: {
-    flex: 1, 
-    alignItems: 'center',
-  },
-  titleText: {
-    fontSize: fontSizes.xlarge,
-    fontWeight: 'bold',
-    textAlign: 'center'
-  },
-  titleTextBis: {
-    alignItems: 'flex-end',
-    fontSize: fontSizes.large,
-    color: 'gray',
-  },
-  icon: {
-    width: fontSizes.iconSmall, 
-    height: fontSizes.iconSmall,
-  },
-  mainView: {
-    flex: 1,
-  },
-  contentView: {
-    flexDirection: 'row',
-  },
-  flatList: {
-    flex: 1,
-    marginHorizontal: horizontalScale(15),
-  },
-  masterLabel: {
-    flex: 1,
-  },
-  modalContainer: {
-    justifyContent: 'flex-end',
-    margin: 0,
-  },
-  modalView: {
-    height: '70%', // Ajustez la hauteur de la modal ici
-    padding: 20,
-    backgroundColor: 'white',
-    borderTopLeftRadius: 20,
-    borderTopRightRadius: 20,
-  },
-  itemContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    padding: 16,
-    borderBottomWidth: 1,
-    borderBottomColor: '#ccc',
-  },
-  textInfoContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 20,
-  },
-  commitMessage: {
-    fontSize: fontSizes.large,
-    fontWeight: 'bold',
-  },
-  commitAuthor: {
-    fontSize: fontSizes.medium,
-  },
+ safeAreaView: {
+   flex: 1,
+ },
+ top: {
+   flexDirection: 'row',
+   alignItems: 'center',
+   justifyContent: 'space-between',
+   marginTop: StatusBar.currentHeight || 0,
+   marginBottom: verticalScale(15),
+ },
+ navigationBack: {
+   marginLeft: horizontalScale(15),
+ },
+ navigationPlaceholder: {
+   width: horizontalScale(90),
+ },
+ titleContainer: {
+   flex: 1,
+   alignItems: 'center',
+ },
+ titleText: {
+   fontSize: fontSizes.xlarge,
+   fontWeight: 'bold',
+   textAlign: 'center',
+ },
+ titleTextBis: {
+   fontSize: fontSizes.large,
+   color: 'gray',
+ },
+ icon: {
+   width: fontSizes.iconSmall,
+   height: fontSizes.iconSmall,
+ },
+ mainView: {
+   flex: 1,
+ },
+ contentView: {
+   flexDirection: 'row',
+ },
+ flatList: {
+   flex: 1,
+   marginHorizontal: horizontalScale(15),
+ },
+ masterLabel: {
+   flex: 1,
+ },
+ modalContainer: {
+   justifyContent: 'flex-end',
+   margin: 0,
+ },
+ modalView: {
+   height: '70%', // Ajustez la hauteur de la modal ici
+   padding: 20,
+   backgroundColor: 'white',
+   borderTopLeftRadius: 20,
+   borderTopRightRadius: 20,
+ },
+ itemContainer: {
+   flexDirection: 'row',
+   justifyContent: 'space-between',
+   alignItems: 'center',
+   padding: 16,
+   borderBottomWidth: 1,
+   borderBottomColor: '#ccc',
+ },
+ textInfoContainer: {
+   flexDirection: 'row',
+   alignItems: 'center',
+   gap: 20,
+ },
+ commitMessage: {
+   fontSize: fontSizes.large,
+   fontWeight: 'bold',
+ },
+ commitAuthor: {
+   fontSize: fontSizes.medium,
+ },
+ commitDate: {
+   fontSize: fontSizes.medium,
+ },
+ searchContainer: {
+   marginHorizontal: horizontalScale(15),
+   marginBottom: verticalScale(15),
+   flexDirection: 'row',
+   alignItems: 'center',
+ },
+ searchInput: {
+   height: verticalScale(40),
+   borderWidth: 1,
+   borderRadius: 10,
+   paddingHorizontal: horizontalScale(10),
+   flex: 1,
+ },
+ sortContainer: {
+   flexDirection: 'row',
+   marginLeft: horizontalScale(10),
+ },
+ sortButton: {
+   paddingVertical: verticalScale(5),
+   paddingHorizontal: horizontalScale(5),
+   borderRadius: 5,
+   borderWidth: 1,
+   borderColor: '#ccc',
+   marginLeft: horizontalScale(5),
+ },
+ selectedSort: {
+   backgroundColor: '#ddd',
+ },
+ sortText: {
+   fontSize: fontSizes.small,
+   color: 'grey',
+ },
 });
+
 
 export default AllCommitsView;
